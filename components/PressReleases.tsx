678d50d18721d2da593f25e5d25b1ff3219f9e86
azkila140 <azkila140@gmail.com>
2026-02-07 06:13:56 +0100

    Critical Hotfix: Restore missing closing brace in PressReleases.tsx to fix build

--- components/PressReleases.tsx
+++ components/PressReleases.tsx
@@ -125,7 +125,7 @@ export function PressReleases() {
                                 </div>
                             </div>
                         </motion.article>
-                    ))
+                    ))}
                 </div>
 
                 {/* View All Button */}
